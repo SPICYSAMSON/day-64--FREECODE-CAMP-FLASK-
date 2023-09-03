7aac369e59b3c6fdfa4a8bf5671c82bcd0d9df06
Third <iamzedthird@gmail.com>
2023-09-03 10:53:23 +0700

    Fetch data from DB and display into webpage (jobs section)

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
 from flask import Flask, render_template, jsonify
+from db import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS =[
-    {
-        'id': 1,
-        'title': "Data Analyst",
-        'location': "Manila, Philippines",
-        'salary': "50,000 pesos"
-    },
-    {
-        'id': 2,
-        'title': "Data Scientist",
-        'location': "Cebu, Philippines",
-        'salary': "70,000 pesos"
-    },
-    {
-        'id': 3,
-        'title': "Frontend Engineer",
-        'location': "Remote",
-    },
-    {
-        'id': 4,
-        'title': "Backend Engineer",
-        'location': "LA, Albay",
-        'salary': "80,000 pesos"
-    },
-]
-
-
 @app.route("/")
 def hello_tres():
+    jobs = load_jobs_from_db()
     return render_template('home.html', 
-                           jobs=JOBS, 
+                           jobs=jobs, 
                            company_name = "Spicysamson")
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    return jsonify(load_jobs_from_db())
 
 
 if __name__== '__main__':
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, text
+from pprint import pprint
 
 # Replace with your correct database connection string
 db_connection_string = "mysql+pymysql://root:HAKDUGERZ69@127.0.0.1:3306/trescareers?charset=utf8mb4"
@@ -6,18 +7,35 @@ db_connection_string = "mysql+pymysql://root:HAKDUGERZ69@127.0.0.1:3306/trescare
 # Create a database engine
 engine = create_engine(db_connection_string)
 
-try:
-    # Connect to the database
-    with engine.connect() as conn:
-        # Execute a SQL query to select all records from the "jobs" table
-        result = conn.execute(text("SELECT * FROM jobs"))
-        
-        # Fetch and print the query results
-        for row in result:
-            print(row)
-except Exception as e:
-    # Handle any exceptions that may occur during the database interaction
-    print("An error occurred:", e)
+
+def load_jobs_from_db():
+    try:
+        # Connect to the database
+        with engine.connect() as conn:
+            # Execute a SQL query to select all records from the "jobs" table
+            result = conn.execute(text("SELECT * FROM jobs"))
+            
+            jobs = []
+            for row in result.all():
+                jobs.append(row._asdict())
+            
+            return jobs
+            
+            # Fetch and print the query results
+            # print("type(result): ", type(result))
+            # result_all = result.all()
+            # print("type(result.all()): ", type(result_all))
+            # first_result=result_all[0]
+            # print("type(first_result): ", type(first_result))
+            
+            # print("\n")
+            # first_result_dict = first_result._asdict()
+            # print("type(first_result_dict): ", type(first_result_dict))
+            # print(first_result_dict)
+            
+    except Exception as e:
+        # Handle any exceptions that may occur during the database interaction
+        print("An error occurred:", e)
 
 
 
