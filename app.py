3ac47cd6ce528afc7a46cc3713db0e3ae7be2f67
Third <iamzedthird@gmail.com>
2023-09-04 14:22:40 +0700

    Create dynamic pages to display each job role and jinja passing variables, also the url_for

--- app.py
+++ app.py
@@ -18,8 +18,12 @@ def list_jobs():
 
 @app.route('/job/<id>')
 def show_job(id):
-    job = load_specificjob_from_db(id)
-    return jsonify(job)
+    specific_job = load_specificjob_from_db(id)         
+    if not specific_job:
+        return "Not found", 404
+    
+    return render_template('jobpage.html',
+                           specific_job=specific_job)
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', debug=True)
